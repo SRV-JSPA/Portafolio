02ed563a7eb6af39d7c07e3c071653c65fd88e58
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-05-22 15:42:26 -0600

    Framer motion added to the project

--- src/components/Entrada.jsx
+++ src/components/Entrada.jsx
@@ -1,4 +1,10 @@
 import perfil from "../Images/perfil.png";
+import {motion} from 'framer-motion'
+
+const contenedor = (delay) => ({
+  hidden: {x: -100, opacity: 0},
+
+})
 
 const Entrada = () => {
   return (
@@ -6,12 +12,12 @@ const Entrada = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1  initial={{x: -100, opacity: 0}} animate={{x: 0, opacity:1}} transition={{duration: 0.5, delay:0.5}} className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Santiago Pereira
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer
-            </span>
+            </motion.span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter" >
               I am a passionate full stack developer with a solid ambition to
               complete projects more efficiently with consistent quality using
