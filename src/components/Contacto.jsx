dce0ba843ee036a35fb1661d54f71b4fc17a6965
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-05-22 15:25:33 -0600

    Created Contacto.jsx and style correction

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,9 +1,19 @@
 
 
 const Contacto = () => {
+  const contacto = {
+    direccion: 'Km 25.5 Los Encinos Condominio, Casa E-18, Choacorral, San Lucas Sacatepéquez.',
+    numero: '+502 3713-8670',
+    email: 'jspereira0402@gmail.com'
+  }
   return (
     <div className="border-b border-neutral-900 pb-20" >
         <h2 className="my-10 text-center text-4xl" >Get in Touch</h2>
+        <div className="text-center tracking-tighter" >
+          <p className="my-4" >{contacto.direccion}</p>
+          <p className="my-4" >{contacto.numero}</p>
+          <a href="#" className="border-b">{contacto.email}</a>
+        </div>
     </div>
   )
 }
