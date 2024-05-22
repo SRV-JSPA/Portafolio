8d0ca4288b93364a96b0b537011bbbe00d6f7d0d
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-05-22 09:30:20 -0600

    Update Estudios.jsx

--- src/components/Estudios.jsx
+++ src/components/Estudios.jsx
@@ -21,6 +21,21 @@ function Estudios () {
   return (
     <div className="border-b border-neutral-900 pb-4" >
         <h2 className="my-20 text-center text-4xl" >Education</h2>
+        <div>
+          {educacion.map((edu, index) => (
+            <div key={index} className="mb-8 flex flex-wrap lg:justify-center" >
+              <div className="w-full lg:w-1/4" >
+                <p className="mb-2 text-sm text-neutral-400" >{edu.year}</p>
+              </div>
+              <div className="w-full max-w-xl lg:w-3/4" >
+                <h6 className="mb-2 font-semibold" >
+                  {edu.grade} - {" "} <span className="text-sm text-purple-100" >{edu.institution}</span>
+                </h6>
+                <p className="mb-4 text-neutral-400" >{edu.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
